06cb5d60f5103218e09ad6780d932d3557035a3c
Macbook <macbook@Macbooks-MacBook-Pro.local>
2022-09-23 18:50:42 -0400

    Included verbose feature into the command line tool

--- visnu.py
+++ visnu.py
@@ -1,10 +1,7 @@
 
 import socket
-import argparse
 import click
-import logging
 import http.client as httplib
-import struct
 
 @click.group()
 def visnu():
@@ -21,21 +18,37 @@ def get_group():
     '''
     pass
 
+def strip_http_headers(http_reply):
+    p = http_reply.find('\r\n\r\n')
+    if p >= 0:
+        return http_reply[p+4:]
+    return http_reply
 
 @click.command(name='price')
 @click.argument('host')
-def get_connection(host):
+@click.option('--verbose/--no-verbose', default=False)
+def get_connection(host, verbose):
     '''
     Gets the connection
     '''
     a1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     a1.connect((socket.gethostbyname(host), 80))
-    a2 = 'GET / HTTP/1.1\r\n' + 'Host: '+host+'\r\n\r\n'
+    a2 = 'GET /status/418 HTTP/1.1\r\n' + 'Host: '+host+'\r\n\r\n'
     p = a1.send(a2.encode())
+    if verbose :
+        header_data_Avail = False
+    else :
+        header_data_Avail = True
     while True :
         data = a1.recv(1024)
-        click.echo(data)
-    
+        if len(data) == 0:     # No more data received, quitting
+            break
+        if(header_data_Avail) :
+            dddd = strip_http_headers(data.decode())
+            header_data_Avail = False
+        else :
+            dddd = data
+        click.echo(dddd)
 
 
 get_group.add_command(get_connection)
